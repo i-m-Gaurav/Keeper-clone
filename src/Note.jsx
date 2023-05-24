16c4fc23aa9a88fcee100aec96a75e2aad24d8fa
unknown <indiagauravkumar@gmail.com>
2023-05-24 13:38:03 +0530

    initial commit

---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return (
+    <div className="note">
+        <h2>Title</h2>
+        <p>This is a simple note.</p>
+    </div>
+    )
+}
+
+export default Note;
